d6593736acca47daffccc20d1934a791156b44d1
aploetz <aaron.ploetz@datastax.com>
2022-11-23 16:41:41 -0600

    allowing default values for connection params

--- testCassandra.py
+++ testCassandra.py
@@ -9,9 +9,14 @@ password="cassandra"
 protocol=4
 
 #check arguments for overrides
-hostname=sys.argv[1]
-username=sys.argv[2]
-password=sys.argv[3]
+if (len(sys.argv) > 1):
+    hostname=sys.argv[1]
+
+if (len(sys.argv) > 2):
+    username=sys.argv[2]
+
+if (len(sys.argv) > 3):
+    password=sys.argv[3]
 
 nodes = []
 nodes.append(hostname)
@@ -20,6 +25,10 @@ auth_provider = PlainTextAuthProvider(username=username, password=password)
 cluster = Cluster(nodes,auth_provider=auth_provider, protocol_version=protocol)
 session = cluster.connect()
 
-rows = session.execute("SELECT key FROM system.local;")
+rows = session.execute("SELECT broadcast_address, host_id, data_center, rack, tokens FROM system.local; -- this is a comment\n")
 for row in rows:
-    print(row[0])
+    print("address: " + row[0])
+    print("host id: " + str(row[1]))
+    print("data center: " + row[2])
+    print("rack: " + row[3])
+    print("tokens: " + str(len(row[4])))
